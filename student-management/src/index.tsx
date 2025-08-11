72d4d24c5cd1a044535aa2abdbc549786157b23f
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-08-11 14:22:48 +0700

    review queryClient

--- student-management/src/index.tsx
+++ student-management/src/index.tsx
@@ -7,12 +7,16 @@ import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 const queryClient = new QueryClient()
+// query client là đối tượng trung tâm của tanStackQuery , quản lú toàn bộ cache và các trạng thái các query trong ứng dụng
+// cho phép thao tác thủ công , linh hoạt với cache
+// quản lý vòng đời , làm mới , xóa , cập nhập dữ liệu
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
+        {/* những thằng nằm trong này sẽ được xài react query */}
         <App />
         <ReactQueryDevtools initialIsOpen={true} />
       </QueryClientProvider>
